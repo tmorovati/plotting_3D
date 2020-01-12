58b4249eed878fe0fe36262a486549e2cb03470b
tmorovati <t.morovati.99@gmail.com>
2020-01-12 10:51:31 -0800

    sample of plot 3D , # different lines with different color

--- plot_9variable.py
+++ plot_9variable.py
@@ -35,11 +35,21 @@ z_a = []
 
 splitted_line = []
 
-'''fig = plt.figure()
-ax1 = fig.add_subplot(111, projection='3d')'''
 
+#3D
+fig = plt.figure()
+
+ax = fig.add_subplot(111, projection='3d')
+
+
+
+
+
+'''
+#2D
 fig = plt.figure()
 ax = plt.subplot(111)
+'''
 
 #set the limitation of the axes
 ax.set_xlim(-100.00 , 100.00)
@@ -60,23 +70,42 @@ def animate(i):
 
     splitted_line[0] = float (splitted_line[0])
     splitted_line[1] = float (splitted_line[1])
-    #splitted_line[2] = float (splitted_line[2])
-    print(splitted_line[0], type(splitted_line))
+    splitted_line[2] = float (splitted_line[2])
 
 
 
+    splitted_line[3] = float (splitted_line[3])
+    splitted_line[4] = float (splitted_line[4])
+    splitted_line[5] = float (splitted_line[5])
+
+    splitted_line[6] = float (splitted_line[6])
+    splitted_line[7] = float (splitted_line[7])
+    splitted_line[8] = float (splitted_line[8])
 
 
 
     x.append(splitted_line[0])
     y.append(splitted_line[1])
-    #z.append(splitted_line[2])
+    z.append(splitted_line[2])
+
+
+    x_v.append(splitted_line[3])
+    y_v.append(splitted_line[4])
+    z_v.append(splitted_line[5])
+
+    x_a.append(splitted_line[6])
+    y_a.append(splitted_line[7])
+    z_a.append(splitted_line[8])
+
+
 
 
 
 
     ax.clear()
-    ax.plot(x , y , z)
+    ax.plot(x , y , z , 'r')
+    ax.plot(x_v,  y_v, z_v , 'y')
+    ax.plot(x_a, y_a, z_a, 'm')
 
 
 ani = animation.FuncAnimation(fig, animate , interval=1 , repeat=True)
